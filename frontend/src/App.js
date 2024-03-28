59179e66ef1bd9ce92d82348c4a3214aa7b765fb
ShashankShekhar07 <shanky74022@gmail.com>
2024-03-28 05:34:00 +0530

    Loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,13 @@ import ProductDetails from "./component/Product/ProductDetails.js"
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search.js";
 import LoginSignUp from './component/User/LoginSignUp.js';
+// import { isAuthenticatedUser } from '../../backend/middleware/auth.js';
+import UserOptions from "./component/layout/Header/UserOptions.js"
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const {isAuthenticated,user} = useSelector((state) => state.user); 
   
   useEffect(() => {
   WebFont.load({
@@ -27,6 +32,7 @@ function App() {
 <Router>
       <Header />
         <Routes>
+          {isAuthenticated && <UserOptions user={user}/>}
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/product/:id" element={<ProductDetails/>}/>
         <Route exact path="/products" element={<Products/>} />
---
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -0,0 +1,83 @@
+import React, { Fragment,useState } from 'react';
+import "./Header.css";
+import { SpeedDial,SpeedDialAction } from "@material-ui/lab";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PersonIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import { useAlert } from 'react-alert';
+import {  useDispatch } from 'react-redux';
+import {logout} from "../../../actions/userAction";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useHistory } from "react-router";
+
+const UserOptions = ({user}) => {
+    const [open,setOpen] = useState(false);
+    let history =useHistory();
+    const alert = useAlert();
+    const dispatch = useDispatch();
+    const options= [
+        {icon : <ListAltIcon/> ,name: "Orders",func: orders},
+        {icon: <PersonIcon/>, name: "Profile",func: account},
+        {icon: <ExitToAppIcon/>, name: "Logout",func: logoutUser},
+    ]
+
+    if(user.role === "admin"){
+        options.unshift({ //Add Dashboard icon if user is admin
+            icon: <DashboardIcon/>,
+            name: "DashBoard",
+            func: dashboard,
+        });
+    }
+
+    function dashboard(){
+        history.push("/dashboard");
+    }
+
+    function orders(){
+        history.push("/orders");
+    }
+
+    function account(){
+        history.push("/account");
+    }
+
+    function logoutUser(){
+        dispatch(logout());
+        alert.success("Logout Successfully");
+    }
+
+  return (
+  <Fragment>
+    <Backdrop open={true} style={{ zIndex: "10"}} />
+        <SpeedDial 
+        ariaLabel='SpeedDial tooltip  example'
+        onClose = {() => setOpen(false)}
+        onOpen={() => setOpen()}
+        open={open}
+        style={{zIndex: "11"}}
+        direction="down"
+        className='speedDial'
+        icon = {
+            <img 
+                className='speedDialIcon'
+                src={user.avatar.url ? user.avatar.url : "/Profile.png"}
+                alt="Profile"
+            />
+        }
+        >
+            {options.map((item) => (
+                <SpeedDialAction 
+                    key={item.name}
+                    icon={item.icon}
+                    tooltipTitle={item.name}
+                    onClick={item.func}
+                />
+            ))}
+
+        </SpeedDial>
+  </Fragment>
+  );
+};
+
+export default UserOptions
